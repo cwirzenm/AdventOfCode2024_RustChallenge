0c79481ec83452251226d63d2a915e1bab2cdabc
mcwirzen <mcwirzen@investcloud.com>
2024-12-03 17:13:13 +0000

    Day 1 and 2

---
+++ src/bin/2024_1.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+fn main() {
+    const INPUT: &str = include_str!("../../resources/day_01.txt");
+    let split: Vec<&str> = INPUT.split("\n").collect::<Vec<&str>>();
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    for pair in split {
+        let line: Vec<&str> = pair.split("   ").collect::<Vec<&str>>();
+        left.push(to_int(line[0]));
+        right.push(to_int(line[1]));
+    }
+    left.sort();
+    right.sort();
+
+    let mut difference: i32 = 0;
+    let it = left.iter().zip(right.iter());
+    for (_i, (x, y)) in it.enumerate() {
+        difference += (x - y).abs()
+    }
+    println!("difference: {:?}", difference);
+
+    let mut similarity: i32 = 0;
+    let mut lookup: HashMap<i32, i32> = HashMap::new();
+    for l in left {
+        if lookup.contains_key(&l) {
+            similarity += lookup.get(&l).unwrap();
+        } else {
+            let count: i32 = right
+                .iter()
+                .filter(|&n| *n == l)
+                .count() as i32;
+            similarity += l * count;
+            lookup.insert(l, l * count);
+        }
+    }
+    println!("similarity: {:?}", similarity);
+}
+
+fn to_int(str: &str) -> i32 {
+    str.parse::<i32>().unwrap()
+}
---
+++ src/bin/2024_2.rs
@@ -0,0 +1,36 @@
+type Report = Vec<usize>;
+
+fn main() {
+    const INPUT: &str = include_str!("../../resources/day_02.txt");
+
+    let count_safe_reports: usize = solve(INPUT);
+    println!("{}", count_safe_reports);
+}
+
+pub fn is_line_valid(line: &Report) -> bool {
+    line.windows(2)
+        .all(|w| 1 <= w[0].abs_diff(w[1]) && w[0].abs_diff(w[1]) <= 3)
+        && (line.is_sorted() || line.iter().rev().is_sorted())
+}
+
+pub fn solve(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|value| value.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .filter(|line: &Report| {
+            is_line_valid(line)
+                || (0..line.len()).any(|skipped| {
+                let new_line: Report = line
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(idx, val)| if idx != skipped { Some(*val) } else { None })
+                    .collect::<Report>();
+                is_line_valid(&new_line)
+            })
+        })
+        .count() as usize
+}
